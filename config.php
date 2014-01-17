588c559391f410dad3d93aadf0442f1a318afbbc
malaizaa <vytautas@eface.lt>
2014-01-17 17:42:09 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$config['apiUrl'] = 'http://ws.local/app_dev.php/';
+$config['apiUrl'] = 'http://ws.kotrynagroup.com/web/';
 $config['username'] = 'testas';
 $config['apiKey'] = '1fa69810cc94008aed4375e1d6fd613f';
