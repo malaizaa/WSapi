4237fac326c22eb81949f61387b3a8890e891553
malaizaa <vytautas@eface.lt>
2014-01-21 15:58:38 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+$config['apiUrl'] = 'http://ws.kotrynagroup.com/web/';
+$config['username'] = '';// api username given by WS administrator
+$config['apiKey'] = ''; // api key given by WS administrator
